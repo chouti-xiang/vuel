85cc7c3fe57071e568927ae37e4a21d869d3ebd4
tuzi <iseedyou@163.com>
2019-03-05 17:18:15 +0800

    增加动态环境变量

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 Vue.config.productionTip = false
-
+console.log(process.env)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
